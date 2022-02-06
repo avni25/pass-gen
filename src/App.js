220190080f1e5b5316293e6901ac70378e76326d
H. Avni Cubukcu <huseyinac@gmail.com>
2022-02-06 19:35:22 +0300

    cc

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PasswordGenerator />
     </div>
   );
 }
+
+class PasswordGenerator extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      password: '',
+    }
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = () => {
+
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>Password Generator</h1>        
+        <button onClick={this.handleClick}>Generate</button>
+      </div>
+    );
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 export default App;
